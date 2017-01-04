753c08dec80ba3a310aab7ab8cf6fdbe58a963fb
omar <thedelarosa@gmail.com>
2017-01-03 22:00:45 -0500

    Updating lastmod date string format to match W3C spec (#3)
    * Updating lastUpdate format to match sitemaps protocol requirements of YYYY-MM-DD for <lastmod>
    
    * Incrementing package.json version to 0.1.2

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ function SitemapWebpackPlugin(base, paths, fileName) {
 }
 
 function GenerateDate() {
-  var date = new Date().toLocaleDateString().split("/");
+  var dateOptions = { day: '2-digit', month: '2-digit', year: 'numeric' };
+  var date = new Date().toLocaleDateString([], dateOptions).split("/");
   var year = date.splice(-1)[0];
   date.splice(0, 0, year);
   var formattedDate = date.join("-");
