66c28a699c302f3d12075177a2b7d16bda03ea0f
Zach Schneider <zach@schneid.io>
2017-07-16 16:18:41 -0400

    Add yarn.lock; replace timecop with timekeeper

--- test/date.js
+++ test/date.js
@@ -1,24 +1,15 @@
-/* global describe it beforeEach afterEach */
+/* global describe it */
 
 import { expect } from 'chai';
-import Timecop from 'timecop';
+import Timekeeper from 'timekeeper';
 
 import generateDate from '../src/date';
 
 describe('generateDate', () => {
-  beforeEach( (done) => {
-    Timecop.install();
-    done();
-  });
-
   it('generates the current date in the correct format', (done) => {
-    Timecop.freeze(new Date(2012, 1, 5, 14, 30));
+    Timekeeper.freeze(new Date(2012, 1, 5, 14, 30));
     expect(generateDate()).to.eq('2012-02-05');
-    done();
-  });
-
-  afterEach( (done) => {
-    Timecop.uninstall();
+    Timekeeper.reset();
     done();
   });
 });
